39f1c2ee37b7caf624f9cd2b16afd9ec446ba168
Verónica <verosgfc11@gmail.com>
2015-12-21 13:20:22 +0100

    Creado un contador de calorias

---
+++ Alimento.java
@@ -0,0 +1,70 @@
+
+/**
+ * Write a description of class Alimento here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alimento
+{
+    private String nombre;
+    private float proteinas;
+    private float carbohidratos;
+    private float grasas;
+    private float calorias;
+    private float gramos;
+    private String componenteMay;
+    
+    /**
+     * Constructor de la clase alimento 
+     */
+    public Alimento(String nombre, float proteinas, float carbohidratos, float grasas)
+    
+    {
+        this.nombre = nombre;
+        this.proteinas = proteinas;
+        this.carbohidratos = carbohidratos;
+        this.grasas = grasas;
+        calorias = (proteinas*4) + (carbohidratos*4) + (grasas*9);
+        if (proteinas > carbohidratos && proteinas > grasas && carbohidratos != grasas)
+        {
+            componenteMay = "Proteinas";
+        }
+        else if(carbohidratos > proteinas && carbohidratos > grasas && proteinas != grasas)
+        {
+            componenteMay = "Carbohidratos";
+        }
+        else if (grasas > proteinas && grasas > carbohidratos && proteinas != carbohidratos)
+        {
+            componenteMay = "Grasas";
+        }
+        else if(proteinas == carbohidratos)
+        {
+            componenteMay = "Proteinas y carbohidratos";
+        }
+        else if(grasas == carbohidratos)
+        {
+            componenteMay = "Grasas y carbohidratos";
+        }
+        else if(proteinas == grasas)
+        {
+            componenteMay = "Proteinas y grasas";
+        }
+        else
+        {
+            componenteMay = "Proteínas, carbohidratos y grasas";
+        }
+    }
+     
+    public void muestraDatos(){
+        
+       
+        System.out.println("Nombre:                                                      " + nombre);
+        System.out.println("Proteínas por cada 100 gramos:                               " + proteinas );
+        System.out.println("Carbohidratos por cada 100 gramos:                           " + carbohidratos);
+        System.out.println("Grasas por cada 100 gramos:                                  " + grasas);
+        System.out.println("Calorías:                                                    " + calorias);
+        System.out.println("Componente/s mayoritario/s:                                  " + componenteMay);
+    }
+    
+}
