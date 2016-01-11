840c2cc066ec3621e2ee6c4ed266cce6128ecaf8
Verónica <verosgfc11@gmail.com>
2016-01-11 12:24:10 +0100

    Creado el método que nos permite comparar a dos usuarios

--- Usuario.java
+++ Usuario.java
@@ -33,7 +33,7 @@ public class Usuario
         totalPro = 0;
         totalCal = 0;
     }
-    
+
     /**
      * Método que le permite al usuario comer un alimento
      */
@@ -44,7 +44,7 @@ public class Usuario
         totalPro = alimento.getProt()*(gramos/100) + totalPro;
         totalCal = alimento.getCal()*(gramos/100)+ totalCal;
     }
-    
+
     /**
      * Método que nos permite ver los datos referidos a comida del usuario
      */
@@ -56,4 +56,34 @@ public class Usuario
         System.out.println("Gramos totales de grasas ingeridas:                    " + totalGra);
         System.out.println("Calorías totales ingeridas:                            " + totalCal);
     }
-}
+    
+    /**
+     * Método que nos permite conocer el nombre del usuario
+     */
+    public String getNombre()
+    {
+        return nombre;
+    }
+    /**
+     * Método que nos permite conocer el total de calorías ingeridas
+     */
+    public float getTotalCal()
+    {
+        return totalCal;
+    }
+    
+    /**
+     * Método que nos permite comparar los datos entre dos usuarios
+     */
+    public void comparacion(Usuario segundoUsuario){
+        if(totalCal == segundoUsuario.getTotalCal())
+            System.out.println("Los dos usuarios han consumido el mismo número de calorías: " + totalCal);
+        else if (getTotalCal() > segundoUsuario.getTotalCal())
+            System.out.println("El usuario: " + nombre + " ha consumido más calorias que el usuario: " + segundoUsuario.getNombre() + " "
+            + "(" + totalCal + " frente a " + segundoUsuario.getTotalCal()+ ")");
+        else
+            System.out.println("El usuario: " + segundoUsuario.getNombre() + " ha consumido más calorias que el usuario: " + nombre + " "
+            + "(" + segundoUsuario.getTotalCal() + " frente a " + totalCal+ ")");
+    }
+    }
+
